3ec140cc0d7d5d817da006ddc4c3d4ffcc20d7c2
jake_xiao <jake_xiao@yeah.net>
2021-10-04 08:04:49 +0800

    add IsPrime

--- math/number.go
+++ math/number.go
@@ -1,16 +1,1 @@
 package math
-
-import "math"
-
-func IsPrime(value int) bool {
-	if value < 2 {
-		return false
-	}
-	end := int(math.Sqrt(float64(value)))
-	for i := 2; i <= end; i++ {
-		if value%i == 0 {
-			return false
-		}
-	}
-	return true
-}
